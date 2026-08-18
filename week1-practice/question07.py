9b055b049ac703c283fef0b326d028a41d6272c5
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-18 14:09:13 +0530

    Enhanced answer 7

--- week1-practice/question07.py
+++ week1-practice/question07.py
@@ -1,40 +1,35 @@
-# Function-Based Word Frequency Analyzer
-
 def count_words(sentence):
-    sentence = sentence.lower()
-    words = sentence.split()
-
+    words = sentence.lower().split()
     word_count = {}
 
-    for i in words:
-        if i in word_count:
-            word_count[i] = word_count[i] + 1
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
         else:
-            word_count[i] = 1
+            word_count[word] = 1
 
-    return word_count
+    return word_count, words
 
 
-sentence = input()
+sentence = input("Enter a sentence: ")
 
-word_frequency = count_words(sentence)
+word_frequency, words = count_words(sentence)
 
-total_words = len(sentence.split())
-
+total_words = len(words)
 unique_words = len(word_frequency)
 
 most_frequent_word = ""
 highest_frequency = 0
 
-for i in word_frequency:
-    if word_frequency[i] > highest_frequency:
-        highest_frequency = word_frequency[i]
-        most_frequent_word = i
+for word, frequency in word_frequency.items():
+    if frequency > highest_frequency:
+        highest_frequency = frequency
+        most_frequent_word = word
 
-print("Word Frequencies:")
+print("\nWord Frequencies:")
 
-for i in word_frequency:
-    print(i, ":", word_frequency[i])
+for word, frequency in word_frequency.items():
+    print(word, ":", frequency)
 
 print("Total Number of Words:", total_words)
 print("Number of Unique Words:", unique_words)
