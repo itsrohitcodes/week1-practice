6b097648f8c9a57fbf2dc862a0e53ef74a2db56e
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-27 15:06:58 +0530

    Question 3 Completed

--- week1-practice/question03.py
+++ week1-practice/question03.py
@@ -5,20 +5,22 @@ courses = {
     "Web": 15
 }
 
-# Display all courses
+
 print("Course Enrollments:")
 for course, enrollment in courses.items():
     print(f"{course}: {enrollment}")
 
-# Ask user for a course
-course_name = input("\nEnter course name: ").strip().title()
 
-if course_name in courses:
-    print("Current Enrollment:", courses[course_name])
+course_name = input().strip().lower()
+
+for course in courses:
+    if course.lower() == course_name:
+        print(f"Current Enrollment: {courses[course]}")
+        break
 else:
     print("Course not found.")
+    
 
-# Analyze enrollments
 total_enrollments = sum(courses.values())
 highest_course = max(courses, key=courses.get)
 lowest_course = min(courses, key=courses.get)
@@ -29,8 +31,8 @@ more_than_20 = [
     if enrollment > 20
 ]
 
-# Display results
-print("\nTotal Enrollments:", total_enrollments)
-print("Course with Highest Enrollment:", highest_course)
-print("Course with Lowest Enrollment:", lowest_course)
-print("Courses Having More Than 20 Students:", more_than_20)
+
+print(f"Total Enrollments: {total_enrollments}")
+print(f"Course with Highest Enrollment: {highest_course}")
+print(f"Course with Lowest Enrollment: {lowest_course}")
+print(f"Courses Having More Than 20 Students: {more_than_20}")
