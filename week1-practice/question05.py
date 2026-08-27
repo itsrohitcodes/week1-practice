24696c5bc13ed694218fc91cf1673468252bd8f4
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-27 15:14:54 +0530

    Question 5 Completed

--- week1-practice/question05.py
+++ week1-practice/question05.py
@@ -1,26 +1,26 @@
-def calculate_salary(basic_salary, bonus_percentage=5):
+def calculate_salary(basic_salary, bonus_percentage):
     bonus_amount = basic_salary * bonus_percentage / 100
     final_salary = basic_salary + bonus_amount
 
     return bonus_amount, final_salary
 
 
-employee_name = input("Enter employee name: ")
-basic_salary = int(input("Enter basic salary: "))
+employee_name = input()
+basic_salary = int(input())
 
 special_bonus = input("Does the employee have a special bonus? ")
 
 if special_bonus.strip().lower() == "yes":
-    bonus_percentage = int(input("Enter bonus percentage: "))
-    bonus_amount, final_salary = calculate_salary(
-        basic_salary, bonus_percentage
-    )
+    bonus_percentage = int(input())
 else:
     bonus_percentage = 5
-    bonus_amount, final_salary = calculate_salary(basic_salary)
 
-print("\nEmployee Name:", employee_name)
-print("Basic Salary:", basic_salary)
-print("Bonus Percentage:", bonus_percentage)
-print("Bonus Amount:", bonus_amount)
-print("Final Salary:", final_salary)
+bonus_amount, final_salary = calculate_salary(
+    basic_salary, bonus_percentage
+)
+
+print(f"Employee Name: {employee_name}")
+print(f"Basic Salary: {basic_salary}")
+print(f"Bonus Percentage: {bonus_percentage}")
+print(f"Bonus Amount: {bonus_amount}")
+print(f"Final Salary: {final_salary}")
