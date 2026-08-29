393bb341629bbc712dfa5e65e06c831d56abf70c
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-29 12:16:47 +0530

    Completed Question 5

--- week1-practice/question05.py
+++ week1-practice/question05.py
@@ -1,10 +1,13 @@
+# QUESTION 05
+
+# Function to calculate salary
 def calculate_salary(basic_salary, bonus_percentage):
     bonus_amount = basic_salary * bonus_percentage / 100
     final_salary = basic_salary + bonus_amount
 
     return bonus_amount, final_salary
 
-
+# Take input from the user
 employee_name = input()
 basic_salary = int(input())
 
@@ -15,10 +18,9 @@ if special_bonus.strip().lower() == "yes":
 else:
     bonus_percentage = 5
 
-bonus_amount, final_salary = calculate_salary(
-    basic_salary, bonus_percentage
-)
+bonus_amount, final_salary = calculate_salary(basic_salary, bonus_percentage)
 
+# Print the results
 print(f"Employee Name: {employee_name}")
 print(f"Basic Salary: {basic_salary}")
 print(f"Bonus Percentage: {bonus_percentage}")
