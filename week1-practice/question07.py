769dd0023e8e473995b1a353ce3b0fb521984f4e
itsrohitcodes <rohitkumar33222@gmail.com>
2026-08-30 13:06:20 +0530

    Completed Question 7

--- week1-practice/question07.py
+++ week1-practice/question07.py
@@ -1,3 +1,6 @@
+# question 07
+
+# Function to count words
 def count_words(sentence):
     words = sentence.lower().split()
     word_count = {}
@@ -10,9 +13,10 @@ def count_words(sentence):
 
     return word_count, words
 
-
+# Input the sentence
 sentence = input()
 
+# Function call
 word_frequency, words = count_words(sentence)
 
 total_words = len(words)
@@ -21,11 +25,13 @@ unique_words = len(word_frequency)
 most_frequent_word = ""
 highest_frequency = 0
 
+# Find the most frequent word
 for word, frequency in word_frequency.items():
     if frequency > highest_frequency:
         highest_frequency = frequency
         most_frequent_word = word
 
+# Print the results
 print("Word Frequencies:")
 
 for word, frequency in word_frequency.items():
